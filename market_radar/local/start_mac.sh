468aa417574a935890ffb7ae82d06ed5165776bb
용모사 <dydrl2001@gmail.com>
2026-09-23 01:19:14 +0900

    Start news feed with local Market Radar

--- market_radar/local/start_mac.sh
+++ market_radar/local/start_mac.sh
@@ -23,7 +23,7 @@ PY
   exit 0
 fi
 docker compose build
-docker compose up -d postgres radar-api kiwoom-feed market-regime
+docker compose up -d postgres radar-api kiwoom-feed market-regime news-feed
 if [ -f data/marketcollector.session ]; then
   docker compose up -d telegram-collector
 else
